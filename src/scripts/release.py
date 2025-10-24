07ed015cb80c9ec6594810330da18e5fcbd83e18
use-the-fork <23747916+use-the-fork@users.noreply.github.com>
2025-10-24 17:57:48 -0400

    chore: Update project version to 0.2.0 and add git-cliff configuration

--- src/scripts/release.py
+++ src/scripts/release.py
@@ -1,10 +1,24 @@
 # todo:
-# uv version --bump patch
-# get version
+
+# Update Commands in documentation
+# uv run python src/scripts/commands_to_md.py
+
+# Update settings in documentation
+# uv run python src/scripts/settings_to_md.py
+
+# Commit here with somekind of default message.
+
+# get the version based on git cliff
+# git cliff --bump --context | jq -r .[0].version
+# Generates somthing like `v0.2.0`
+
+# Then add the version
+# git tag {version}
+
+# Update the changelog
 # git-cliff --tag {version} -o
 
+# Update uv version
+# uv version {version}
 
-# git tag v0.1.11
 # git push --tags
-
-# t
