e74765455d2d3b397165d3340d0af0fd184c874f
use-the-fork <23747916+use-the-fork@users.noreply.github.com>
2025-12-28 12:47:30 -0500

    refactor: simplify import and __all__ in multiple modules

--- src/byte/core/utils/__init__.py
+++ src/byte/core/utils/__init__.py
@@ -1,12 +1,19 @@
-from byte.core.utils.dump import dd, dump
-from byte.core.utils.extract_content_from_message import extract_content_from_message
-from byte.core.utils.get_language_from_filename import get_language_from_filename
-from byte.core.utils.get_last_message import get_last_message
-from byte.core.utils.list_to_multiline_text import list_to_multiline_text
-from byte.core.utils.slugify import slugify
-from byte.core.utils.value import value
+"""Utilities."""
 
-__all__ = [
+from typing import TYPE_CHECKING
+
+from byte._import_utils import import_attr
+
+if TYPE_CHECKING:
+    from byte.core.utils.dump import dd, dump
+    from byte.core.utils.extract_content_from_message import extract_content_from_message
+    from byte.core.utils.get_language_from_filename import get_language_from_filename
+    from byte.core.utils.get_last_message import get_last_message
+    from byte.core.utils.list_to_multiline_text import list_to_multiline_text
+    from byte.core.utils.slugify import slugify
+    from byte.core.utils.value import value
+
+__all__ = (
     "dd",
     "dump",
     "extract_content_from_message",
@@ -15,4 +22,27 @@ __all__ = [
     "list_to_multiline_text",
     "slugify",
     "value",
-]
+)
+
+_dynamic_imports = {
+    "dd": "dump",
+    "dump": "dump",
+    "extract_content_from_message": "extract_content_from_message",
+    "get_language_from_filename": "get_language_from_filename",
+    "get_last_message": "get_last_message",
+    "list_to_multiline_text": "list_to_multiline_text",
+    "slugify": "slugify",
+    "value": "value",
+}
+
+
+def __getattr__(attr_name: str) -> object:
+    module_name = _dynamic_imports.get(attr_name)
+    parent = __spec__.parent if __spec__ is not None else None
+    result = import_attr(attr_name, module_name, parent)
+    globals()[attr_name] = result
+    return result
+
+
+def __dir__() -> list[str]:
+    return list(__all__)
